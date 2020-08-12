a64947b732b5c02bafdeda9f23186c65b884898c
phb <362364468@qq.com>
2020-08-12 11:17:17 +0800

    创建项目

---
+++ src/page/Login/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="maincontain">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        message:"Hello World",
+        visible:false
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+@height:500px;
+.maincontain{
+  position: relative;
+  margin: 0 auto;
+  background-color: #A6A9AD;
+  width: 600px;
+  height: @height;
+  top: calc(50vh - @height / 2);
+}
+</style>
